13fb52ecfe10083bfb35cad49aa9d8b879d35389
Shlok Mohanty <59838234+ShlokMohanty@users.noreply.github.com>
2022-06-19 22:20:11 +0530

    comments added

--- reactive-routes-quickstart/src/main/java/org/acme/reactive/routes/MyRouteRegistar.java
+++ reactive-routes-quickstart/src/main/java/org/acme/reactive/routes/MyRouteRegistar.java
@@ -6,10 +6,10 @@ import javax.enterprise.event.Observes;
 import io.vertx.ext.web.Router;
 
 @ApplicationScoped
-public class MyRouteRegistar {
+public class MyRouteRegistar {//MyRouteRegister class used 
 
-    public void init(@Observes Router router) {
-        router.get("/my-route").handler(rc -> rc.response().end("Hello from my route"));
+    public void init(@Observes Router router) {//init method created 
+        router.get("/my-route").handler(rc -> rc.response().end("Hello from my route"));//get method called for getting /my-route that will handle teh response hello from mjy route
     }
 
 }
