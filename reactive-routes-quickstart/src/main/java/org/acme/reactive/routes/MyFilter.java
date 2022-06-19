bcc7c5190d35b3acea9ddfb83c5d5dccde908e8c
Shlok Mohanty <59838234+ShlokMohanty@users.noreply.github.com>
2022-06-19 22:18:21 +0530

    comments added

--- reactive-routes-quickstart/src/main/java/org/acme/reactive/routes/MyFilter.java
+++ reactive-routes-quickstart/src/main/java/org/acme/reactive/routes/MyFilter.java
@@ -6,11 +6,11 @@ import javax.enterprise.event.Observes;
 import io.quarkus.vertx.http.runtime.filters.Filters;
 
 @ApplicationScoped
-public class MyFilter {
+public class MyFilter { //created a filter class with the public specifier 
 
-    public void registerMyFilter(@Observes Filters filters) {
+    public void registerMyFilter(@Observes Filters filters) {// function of registermyfilter register operation is used  
         filters.register(rc -> {
-            rc.response().putHeader("X-Header", "intercepting the request");
+            rc.response().putHeader("X-Header", "intercepting the request"); //function call back method is used for getting the response as putting header "x-header","intercepting the request"
             rc.next();
         }, 100);
     }
