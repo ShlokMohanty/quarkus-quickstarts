d4912f0a81ea1db56ca4964c12f605aa87b985f3
Shlok Mohanty <59838234+ShlokMohanty@users.noreply.github.com>
2022-06-19 22:14:03 +0530

    added comments for the betterment of understanding the code

--- reactive-routes-quickstart/src/main/java/org/acme/reactive/routes/MyDeclarativeRoutes.java
+++ reactive-routes-quickstart/src/main/java/org/acme/reactive/routes/MyDeclarativeRoutes.java
@@ -7,16 +7,16 @@ import io.vertx.core.http.HttpMethod;
 import io.vertx.ext.web.RoutingContext;
 
 @ApplicationScoped
-public class MyDeclarativeRoutes {
+public class MyDeclarativeRoutes { //created a public class with the name of MyDeclarativeRoutes
 
-    @Route(path = "/", methods = Route.HttpMethod.GET)
-    public void handle(RoutingContext rc) {
+    @Route(path = "/", methods = Route.HttpMethod.GET)//methods we used ofr using the http method so we called the get operation 
+    public void handle(RoutingContext rc) {//function created for handling the context will get a response with a hello message 
         rc.response().end("hello");
     }
 
     @Route(path = "/hello", methods = Route.HttpMethod.GET)
     public void greetings(RoutingContext rc) {
-        String name = rc.request().getParam("name");
+        String name = rc.request().getParam("name");// will make a request to get the name of the parameter as name name is nothing then print world otherwise respond with a name 
         if (name == null) {
             name = "world";
         }
